1375f5127a26a5481f26e21f687194095490d20c
gkgy（天使的龙蛋） <gkgy@qq.com>
2016-11-09 22:11:53 +0800

    Add files via upload

---
+++ twoonetest.py
@@ -0,0 +1,15 @@
+﻿import pprint
+info = '''SCENE I. Yorkshire. Gaultree Forest.
+Enter the ARCHBISHOP OF YORK, MOWBRAY, LORD HASTINGS, and others
+ARCHBISHOP OF YORK
+What is this  forest call'd?
+HASTINGS
+'Tis Gaultree
+Of forge
+His foes are so enrooted with his friends'''
+count = { }
+for character in info.upper():
+    count[character]=count.get(character,0)+1
+
+value = pprint.pformat(count)
+print(value)
